db4e72a02b1ecdb9a6b780a53d5f48577b9a379f
matheus <matheusrodrig16@gmail.com>
2024-07-11 09:44:50 -0300

    Docs: remove

--- rag.py
+++ rag.py
@@ -1,43 +1,33 @@
-# from langchain_community.vectorstores import Chroma
-
-# from langchain_community.chat_models import ChatOllama
-# from langchain_community.embeddings import FastEmbedEmbeddings
-# from langchain.schema.output_parser import StrOutputParser
-# from langchain_community.document_loaders import PyPDFLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.schema.runnable import RunnablePassthrough
-# from langchain.prompts import PromptTemplate
-# from langchain.vectorstores.utils import filter_complex_metadata
-
 from langchain_community.vectorstores import Chroma
 from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-
 from langchain.schema.output_parser import StrOutputParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores import utils as chromautils
 
-
-
 class ChatPDF:
     vector_store = None
     retriever = None
     chain = None
 
-    def __init__(self):
-        self.model = ChatOllama(model="tinyllama")
+    def __init__(self, temperature=0.02, top_p=0.05, top_k=1):
+        self.model = ChatOllama(
+            model="llama3:8b",
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k
+        )
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
         self.prompt = PromptTemplate.from_template(
             """
-            <s> [INST] You are an assistant for question-answering tasks. Use the following pieces of retrieved context 
-            to answer the question. If you don't know the answer, just say that you don't know. Use three sentences
-             maximum and keep the answer concise. [/INST] </s> 
-            [INST] Question: {question} 
-            Context: {context} 
-            Answer: [/INST]
+            You are an assistant for question-answering tasks. Use only the provided context to answer the question.
+            If the answer is not in the context, respond with "I don't know." Provide a concise answer.
+            Question: {question}
+            Context: {context}
+            Answer:
             """
         )
 
@@ -51,7 +41,7 @@ class ChatPDF:
             search_type="similarity_score_threshold",
             search_kwargs={
                 "k": 3,
-                "score_threshold": 0.5,
+                "score_threshold": 0.7,
             },
         )
 
@@ -64,7 +54,11 @@ class ChatPDF:
         if not self.chain:
             return "Please, add a PDF document first."
 
-        return self.chain.invoke(query)
+        response = self.chain.invoke(query)
+        
+        if "I don't know" in response:
+            return "The information you are looking for is not available in the provided PDF."
+        return response
 
     def clear(self):
         self.vector_store = None
